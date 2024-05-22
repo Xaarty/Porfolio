ca298fed718b51a1a01a39872d556977a3e1ca9d
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-05-22 23:48:22 +0200

    .

--- Portfolio/src/components/projets.jsx
+++ Portfolio/src/components/projets.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Projets() {
     return (
         <section className="projets">
@@ -8,7 +10,13 @@ export default function Projets() {
             <h2>Mes projets</h2>
             <div className="projets_individual">
                 <h3>Création d'une application web de location immobilière avec React</h3>
-                <div className="projets_individual_card"></div>
+                <div className="projets_individual_card">
+                    <Link to="/" className="projets_individual_card_ancre"><img src="./immobilier-react.webp" alt="projet immobilier react" className="projets_individual_card_ancre_img" /></Link>
+                    <div className="projets_individual_card_txt">
+                        <div className="projets_individual_card_txt_titre"><p>Application de location immobilière</p></div>
+                        <div className="projets_individual_card_txt_annee"><p>2024</p></div>
+                    </div>
+                </div>
                 <div className="projets_individual_tags"></div>
             </div>
             <div className="projets_individual">
