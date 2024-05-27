a107478b6afa405d3bd510b762f209c97f65c2a1
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-05-27 17:11:04 +0200

    .

--- Portfolio/src/routes/projet1.jsx
+++ Portfolio/src/routes/projet1.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export default function Location () {
     return (
         <section className="block">
             <div className="block_title">
                 <h1>Création d'une application web de location immobilière avec React</h1>
-                <div></div>
+                <div className="block_title_bar"></div>
+                <div className="block_title_cards">
+                    <div className="block_title_cards_individual">React</div>
+                    <div className="block_title_cards_individual">React Router</div>
+                </div>
             </div>
             <div className="block_imglink">
-                <Link to="/location-immobiliere" className="projets_individual_card_ancre"><img src="./immobilier-react.webp" alt="projet immobilier react" className="projets_individual_card_ancre_img" /></Link>
-                <div><Link to="" >Projet sur GitHub</Link></div>
+                <img src="./immobilier-react.webp" alt="projet immobilier react" className="projets_individual_card_ancre_img block_imglink_img" />
+                <div className="block_imglink_card"><NavLink to="https://github.com/Xaarty/Projet8" className="block_imglink_card_git">Projet sur GitHub</NavLink></div>
             </div>
             <div className="block_resume">
                 <h2>Quel est la nature de ce projet ?</h2>
