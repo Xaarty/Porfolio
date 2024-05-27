98002085ea8997d7881903f8d91d54404c66eb70
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-05-27 19:28:35 +0200

    .

--- Portfolio/src/routes/projet1.jsx
+++ Portfolio/src/routes/projet1.jsx
@@ -6,6 +6,7 @@ export default function Location () {
         <section className="block">
             <div className="block_title">
                 <h1>Création d'une application web de location immobilière avec React</h1>
+                <h2>Projet OpenClassrooms</h2>
                 <div className="block_title_bar"></div>
                 <div className="block_title_cards">
                     <div className="block_title_cards_individual">React</div>
@@ -18,7 +19,20 @@ export default function Location () {
             </div>
             <div className="block_resume">
                 <h2>Quel est la nature de ce projet ?</h2>
-                <p></p>
+                <div className="block_resume_def">
+                    <p>Ce projet a lieu dans le cadre de la formation OpenClassrooms de développeur web. Il requiert d'implémenter le front-end de KASA à l'aide de React et React Router.</p>
+                    <p>Kasa est un site de location immobilier. Il à une page d'acceuil avec des logements en photo, lorsque l'utilisateur clique sur la photo du logement, il est redirigé vers la fiche du logement.</p>
+                    <div className="block_resume_def_imgblock"><img src="./immobilier-react-logement.webp" alt="exemple de logement sur le site kasa" className="block_resume_def_imgblock_img"></img></div>
+                    <p className="before_list">La fiche logement comporte les détails de la location, à savoir :</p>
+                    <ul>
+                        <li>Ou elle se situe</li>
+                        <li>Un caroussel avec toutes les photos de l'appartement</li>
+                        <li>Une photo du bailleur</li>
+                        <li>Une note</li>
+                        <li>Un descriptif de l'appartement</li>
+                        <li>Les équipements disponible</li>
+                    </ul>
+                </div>
             </div>
             <div className="block_stacks">
                 <h3>Quels stacks et compétences j'ai pu développer ?</h3>
