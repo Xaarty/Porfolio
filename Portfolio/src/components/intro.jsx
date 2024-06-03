c174a53c16eb023ede3931eb949640a286f098ca
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-06-03 19:02:28 +0200

    framer motion

---
+++ Portfolio/src/components/intro.jsx
@@ -0,0 +1,29 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { motion, useScroll,useTransform } from "framer-motion";
+import { useRef } from "react";
+
+
+export default function Intro() {
+    const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "65%"]);
+  
+    return (
+           <section className="intro" ref={ref}>
+                <Parallax pages={1} style={{ top: '0', left: '0' }} className="intro_parallax">
+                    <ParallaxLayer offset={0} speed={2.5} className="intro_parallax_block">
+                        <img src="./cloud.webp" alt="" className="intro_parallax_block_img"  />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={0} speed={10} className="intro_parallax_block">
+                        <motion.div style={{y: textY}} className="intro_parallax_block_img intro_parallax_block_txt">
+                            <h1 className="title">P O R T F O L I O</h1>
+                            <h2 className="subtitle">Développer Web</h2>
+                        </motion.div>
+                    </ParallaxLayer>
+                </Parallax>
+           </section>
+    )
+}
--- Portfolio/src/routes/home.jsx
+++ Portfolio/src/routes/home.jsx
@@ -1,10 +1,13 @@
 import Propos from "../components/a-propos"
 import Portrait from "../components/portrait"
 import Projets from "../components/projets"
+import Intro from "../components/intro"
+
 
 export default function Home () {
     return (
         <div className="home-content">
+            <Intro />
             <Portrait />
             <Propos />
             <Projets />
