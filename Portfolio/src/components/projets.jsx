4f4c7be864fe70c4f5f8ad5ce54a0f08e36051b5
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-06-10 22:32:46 +0200

    avancées caroussel

--- Portfolio/src/components/projets.jsx
+++ Portfolio/src/components/projets.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom"
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { motion, useScroll,useTransform } from "framer-motion";
 import { useRef } from "react";
+import Caroussel from "./caroussel";
+import JsonData from "../caroussel.json"
+
 
 export default function Projets() {
-    const ref = useRef(null);
+    
+    const ref = useRef(null);   
     const { scrollYProgress } = useScroll({
       target: ref,
       offset: ["start start", "end start"],
     });
     const textY = useTransform(scrollYProgress, [0, 1], ["0%", "35%"]);
     const textY2 = useTransform(scrollYProgress, [0, 1], ["0%", "35%"]);
+
     return (
         <section className="projets" ref={ref}>
             <Parallax pages={1} style={{ top: '0', left: '0' }} className="projets_parallax">
@@ -23,29 +27,11 @@ export default function Projets() {
                     </motion.div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={10} className="projets_parallax_block">
-                    <motion.div style={{y: textY2}} className="projets_parallax_block_project1">
-                        <h3 className="projets_parallax_block_projet1_h3">Création d'une application web de location immobilière avec React</h3>
-                        <div className="projets_parallax_block_projet1_block">
-                            <Link to="/location-immobiliere" className="projets_parallax_block_projet1_block_ancre"><img src="./immobilier-react.webp" alt="projet immobilier react" className="projets_parallax_block_projet1_block_ancre_img" /></Link>
-                            <div className="projets_parallax_block_projet1_block_txt">
-                                <div className="projets_parallax_block_projet1_block_txt_titre"><p>Application de location immobilière</p></div>
-                                <div className="projets_parallax_block_projet1_block_txt_annee"><p>2024</p></div>
-                            </div>
-                        </div>
+                    <motion.div style={{y: textY2}} className="projets_parallax_block_projet">
+                        <Caroussel projets={JsonData} />
                     </motion.div>
                 </ParallaxLayer>
-                <ParallaxLayer offset={0} speed={10} className="projets_parallax_block">
-                    <motion.div style={{y: textY2}} className="projets_parallax_block_project1">
-                    <h3 className="projets_parallax_block_projet1_h3">Implémentation du front-end d'une application bancaire avec React</h3>
-                    <div className="projets_parallax_block_projet1_block">
-                        <Link to="/bank" className="projets_parallax_block_projet1_block_ancre"><img src="./bank.webp" alt="projet bancaire react" className="projets_individual_card_ancre_img" /></Link>
-                        <div className="projets_parallax_block_projet1_block_txt">
-                            <div className="projets_parallax_block_projet1_block_txt_titre"><p>Application Bancaire</p></div>
-                            <div className="projets_parallax_block_projet1_block_txt_annee"><p>2024</p></div>
-                        </div>
-                    </div>
-                    </motion.div>
-                </ParallaxLayer>
+               
             </Parallax>
             {/* <h2></h2>
             <div className="projets_individual">
@@ -67,7 +53,12 @@ export default function Projets() {
         </section>
     )
 }
-
-{/* <section className="intro" ref={ref}>
-                
-           </section> */}
+{/* <ParallaxLayer offset={0} speed={10} className="projets_parallax_block">
+<motion.div style={{y: textY2}} className="projets_parallax_block_project1">
+<h3 className="projets_parallax_block_projet1_h3">Implémentation du front-end d'une application bancaire avec React</h3>
+<div className="projets_parallax_block_projet1_block">
+    <Link to="/bank" className="projets_parallax_block_projet1_block_ancre"><img src="./bank.webp" alt="projet bancaire react" className="projets_individual_card_ancre_img" /></Link>
+   
+</div>
+</motion.div>
+</ParallaxLayer> */}
